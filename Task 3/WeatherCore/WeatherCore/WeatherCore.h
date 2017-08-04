497a4646c948ee4feddb14b38067c11fcdcdff12
Nghia Tran <vinhnghiatran@gmail.com>
2017-08-04 16:38:02 +0700

    Create WeatherCore project

---
+++ Task 3/WeatherCore/WeatherCore/WeatherCore.h
@@ -0,0 +1,19 @@
+//
+//  WeatherCore.h
+//  WeatherCore
+//
+//  Created by Nghia Tran on 8/4/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeatherCore.
+FOUNDATION_EXPORT double WeatherCoreVersionNumber;
+
+//! Project version string for WeatherCore.
+FOUNDATION_EXPORT const unsigned char WeatherCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherCore/PublicHeader.h>
+
+
