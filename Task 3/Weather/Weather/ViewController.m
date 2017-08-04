9a3a7818633a3e42e846b44b2f60329acdbae824
Nghia Tran <vinhnghiatran@gmail.com>
2017-08-04 16:18:57 +0700

    Init Project Weather

---
+++ Task 3/Weather/Weather/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Weather
+//
+//  Created by Nghia Tran on 8/4/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Task 3/Weather/WeatherTests/WeatherTests.m
@@ -0,0 +1,39 @@
+//
+//  WeatherTests.m
+//  WeatherTests
+//
+//  Created by Nghia Tran on 8/4/17.
+//  Copyright © 2017 Nghia Tran. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WeatherTests : XCTestCase
+
+@end
+
+@implementation WeatherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
